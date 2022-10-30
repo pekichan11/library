fa86c9ff6bcba6ad8b2789e48eda6907c1b1ccb0
pekichan11 <samuel.peki@gmail.com>
2022-10-30 16:18:13 +0100

    spacing

--- login.php
+++ login.php
@@ -6,24 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in</title>
 </head>
-<body>
-    <form>
-        <div>
-            <label for="email"/>
-            <input id="email" class="" type="email" name="email" required autofocus />
-        </div>
+    <body>
+        <form>
+            <div>
+                <label for="email"/>
+                <input id="email" class="" type="email" name="email" required autofocus />
+            </div>
 
-        <div>
-            <label for="password"/>
-            <input id="password" class="" type="pasword" name="password" required />
-        </div>
+            <div>
+                <label for="password"/>
+                <input id="password" class="" type="pasword" name="password" required />
+            </div>
 
-        <div>
+            <div>
                 <label for="remember_me" class="">
                     <input id="remember_me" type="checkbox" class="" name="remember">
                     <span class="ml-2 text-sm text-gray-600">Remember me ?</span>
                 </label>
             </div>
-    </form>
-</body>
+        </form>
+    </body>
 </html>
